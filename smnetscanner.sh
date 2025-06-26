78169e4dedf61265b13fc83a76a00d2d65420987
Asif Agaria <19821+lyarinet@users.noreply.github.com>
2025-06-26 14:29:20 +0500

    feat: add support for scanning all network interfaces
    - Added option to scan all available network interfaces by typing 'all'
    - Interfaces are discovered using `ip -o -f inet addr show`
    - IP ranges are extracted per-interface and scanned with nmap
    - Merged and deduplicated all IPs from all scanned interfaces
    - Existing output table preserved with collected results
    - Improved user input validation for interface selection

--- smnetscanner.sh
+++ smnetscanner.sh
@@ -1,35 +1,67 @@
 #!/bin/bash
-# (c) CompuMatter, LLC, ServerMatter
+# (c) Lyarinet, Asifagaria
 # no warranty expressed or implied - use as is.
 # See https://www.youtube.com/watch?v=4hjskxkapYo
 
-# versioned here: https://gist.github.com/kwmiebach/6565ad1b43f3c781b23fce6ba80f59ea
-# history of the gist:
-# 2023-09-28 downloaded from https://cloud.compumatter.biz/s/fxfYM9SkamBtGqG - unchanged
+# versioned here: https://github.com/lyarinet/smnetscanner.sh.git
+# history of the github:
+# 2025-06-26 downloaded from https://github.com/lyarinet/smnetscanner.sh.git - Modified to selected eth?
 
 # purpose of this script:
 # Can't use angryipscanner from a command line and haven't been able to find anything else that gives you what you're looking for?   
 # This nmap based bash script might just be what you're looking for. 
 
+# Check for nmap installation
 if ! which nmap >/dev/null; then
     echo "nmap is not installed - this script requires it"
     echo "It can be installed with - apt install nmap"
-    return;
+    exit 1
 fi
-# Check if the script is being run as root (EUID = 0)
+
+# Check if script is run as root
 if [ "$EUID" -ne 0 ]; then
     echo "This script must be run as root user or with sudo"
-    return;
+    exit 1
 fi
+
 lease_file_location="/var/lib/dhcp/dhcpd.leases"
 
-# will return br0, eth0, eno1 or whatever the default Interface is
-default_interface=$(ip route | awk '/default/ {print $5}')
-ip_and_cidr=$(ip -o -f inet addr show $default_interface | awk '{print $4}')
-ip_range=$(echo $ip_and_cidr | sed 's/\.[0-9]*\//.0\//')
+# Prompt the user to select a network interface or scan all
+echo "Available network interfaces:"
+ip -o link show | awk -F': ' '{print $2}' | grep -v "lo"
+echo "Type 'all' to scan all interfaces"
+read -p "Enter the interface to scan (e.g., eth0, ens33, or all): " selected_interface
 
-echo -e "\nRunning nmap -sn $ip_range to get a list of all IP addresses\n"
-readarray -t ips < <(nmap -sn $ip_range | awk '/Nmap scan report/{gsub(/[()]/,""); print $NF}' | sort -t . -n -k 1,1 -k 2,2 -k 3,3 -k 4,4)
+interfaces=()
+if [ "$selected_interface" == "all" ]; then
+    mapfile -t interfaces < <(ip -o -f inet addr show | awk '!/ lo / {print $2}' | sort -u)
+else
+    if ! ip link show "$selected_interface" > /dev/null 2>&1; then
+        echo "Invalid network interface: $selected_interface"
+        exit 1
+    fi
+    interfaces=("$selected_interface")
+fi
+
+# Collect all IPs to scan
+all_ips=()
+for iface in "${interfaces[@]}"; do
+    ip_and_cidr=$(ip -o -f inet addr show "$iface" | awk '{print $4}')
+
+    if [ -z "$ip_and_cidr" ]; then
+        echo "No IP address found on $iface, skipping."
+        continue
+    fi
+
+    ip_range=$(echo "$ip_and_cidr" | sed 's/\.[0-9]*\//.0\//')
+
+    echo -e "\nRunning nmap -sn $ip_range on interface $iface\n"
+    mapfile -t iface_ips < <(nmap -sn "$ip_range" | awk '/Nmap scan report/{gsub(/[()]/,"",$NF); print $NF}')
+    all_ips+=("${iface_ips[@]}")
+done
+
+# Remove duplicate IPs and sort
+IFS=$'\n' read -r -d '' -a ips < <(printf "%s\n" "${all_ips[@]}" | sort -u && printf '\0')
 
 # Set column widths
 col1=14
@@ -40,34 +72,28 @@ col5=30
 
 echo -e "Checking each IP address for Hostname, MAC, Workgroup or Domain, Manufacturer info\n"
 
-# Format the output
+# Format header
 printf "%-${col1}s | %-${col2}s | %-${col3}s | %-${col4}s | %-${col5}s \n" "IP" "MAC" "HOSTNAME" "WG-DOMAIN" "MANUFACTURER"
-printf "%-${col1}s | %-${col2}s | %-${col3}s | %-${col4}s | %-${col5}s \n" "$(printf '%.s-' {1..13})" "$(printf '%.s-' {1..17})" "$(printf '%.s-' {1..17})" "$(printf '%.s-' {1..15})" "$(printf '%.s-' {1..30})"
+printf "%-${col1}s | %-${col2}s | %-${col3}s | %-${col4}s | %-${col5}s \n" \
+  "$(printf '%.s-' {1..13})" "$(printf '%.s-' {1..17})" "$(printf '%.s-' {1..17})" "$(printf '%.s-' {1..15})" "$(printf '%.s-' {1..30})"
 
 for IP in "${ips[@]}"
 do
-  # Run the nmap command for the current IP
-  OUTPUT="$(nmap --script nbstat.nse -p 137,139 $IP)"
-  # Extract the necessary information
+  OUTPUT="$(nmap --script nbstat.nse -p 137,139 "$IP")"
   MAC=$(echo "$OUTPUT" | grep 'MAC Address' | awk '{print $3}')
   HOSTNAME=$(echo "$OUTPUT" | grep '<20>.*<unique>.*<active>' | awk -F'[|<]' '{print $2}' | tr -d '_' | xargs)
   WG_DOMAIN=$(echo "$OUTPUT" | grep -v '<permanent>' | grep '<00>.*<group>.*<active>' | awk -F'[|<]' '{print $2}' | tr -d '_' | xargs)
   MANUFACTURER=$(echo "$OUTPUT" | grep 'MAC Address' | awk -F'(' '{print $2}' | cut -d ')' -f1)
 
-  # if a dhcp server leases file exists on this machine, we will query it for a hostname if not already returned by nmap
   if [ -f "$lease_file_location" ]; then
-    # If HOSTNAME is empty, fetch from dhcpd.leases
     if [ -z "$HOSTNAME" ]; then
       HOSTNAME=$(awk -v ip="$IP" '$1 == "lease" && $2 == ip {f=1} f && /client-hostname/ {print substr($2, 2, length($2) - 3); exit}' "$lease_file_location" | cut -c 1-15)
-
-      # Append an asterisk (*) if HOSTNAME has a value
       if [ -n "$HOSTNAME" ]; then
         HOSTNAME="$HOSTNAME *"
       fi
     fi
   fi
 
-  # Print a row of data for the current IP
   printf "%-${col1}s | %-${col2}s | %-${col3}s | %-${col4}s | %-${col5}s \n" "$IP" "$MAC" "$HOSTNAME" "$WG_DOMAIN" "$MANUFACTURER"
 done
 
@@ -75,8 +101,4 @@ if [ -f "$lease_file_location" ]; then
   echo -e "\n* to the right of hostname indicates the hostname could not be acquired from nmap so was pulled from $lease_file_location\n"
 fi
 
-# we are grateful if you would leave our short tagline attached
-echo -e "This network scanner script is provided free of charge by ServerMatter\n"
-# Disclaimer: This script is provided as-is with no warranty and no responsibility.
-# The author and contributors shall not be liable for any direct, indirect, incidental,
-# special, exemplary, or consequential damages arising from the use of this script.
+echo -e "This network scanner script is provided free of charge by Asifagaria\n"
